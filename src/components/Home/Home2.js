a23b2485cbd3d88d11024bfd7c924a7753fd8d56
SHUBHAM MISHRA <60005297+TonyStark0801@users.noreply.github.com>
2023-11-28 17:32:06 +0000

    Removed unecessary comments

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -14,44 +14,7 @@ function Home2() {
     <Container fluid className="home-about-section" id="about">
       <Container>
           <div md={8} className="home-about-description">
-            {/* <h1 style={{ fontSize: "2.6em" }}>
-            Allow Me to  <span className="purple"> Introduce </span> Myself 
-            </h1> */}
-            {/* <div class="night shooting_star" ></div> */}
             <p className="home-about-body">
-             
-              {/* I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i> */}
-            
             Crafting Digital Marvels! I'm Shubham Mishra, 
             a coding virtuoso driven by a relentless quest for innovation. With a knack for problem-solving, 
             I engineer extraordinary solutions that push the boundaries of what's possible. From architecting 
