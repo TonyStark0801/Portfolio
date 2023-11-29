43ba42c9e74c64fb31d9683ba2b2cc2d214f917d
SHUBHAM MISHRA <60005297+TonyStark0801@users.noreply.github.com>
2023-11-29 19:10:39 +0000

    Seding Email complete

--- src/components/Home/connect.js
+++ src/components/Home/connect.js
@@ -1,6 +1,5 @@
 import { Container } from 'react-bootstrap';
 import { Form, Input, TextArea, Button } from 'semantic-ui-react';
-import Email from 'https://smtpjs.com/v3/smtp.js';
 
 import Particle from "../Particle";
 import { useState } from 'react';
@@ -9,19 +8,35 @@ import { useState } from 'react';
 
 function Connect() {
   const [formState,setFormState]= useState({});
-  const changeHandler = (e)=> {
-    setFormState({...formState,[e.target.name]:e.target.value})
+
+  const changeHandler = (event)=> {
+    setFormState({...formState,[event.target.name]:event.target.value});
+  }
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    const config = {
+      SecureToken: 'de22f1b4-39a8-4a7c-b8d4-3897ef2580e2',
+      To : 'anonymousperson6864@gmail.com',
+      From : formState.email,
+      Subject : "This is my Contact form",
+      Body : `${formState.name} connect to you over email. My phone number ${formState.phone}`
+    };
+
+    if(window.Email){
+      window.Email.send(config).then(()=>alert("Message sent successfully!"));
+    }
   }
 
     return (
       <section>
 <Particle/>
         <div className='contact' >
-          <Form  >
-          <input type="text" id= "name" placeholder='Your Name' value={""}  onChange={changeHandler} required></input>
-          <input type="email" id= "email" placeholder='Email id'  value={""}  onChange={changeHandler} required></input>
-          <input type="text" id= "phone" placeholder='Phone no.' value={""}  onChange={changeHandler} required></input>
-          <textarea id = "message" rows="4" placeholder='How can I help you?' value={""}  onChange={changeHandler}></textarea>
+          <Form onSubmit={submitHandler} >
+          <input type="text" name= "name" placeholder='Your Name' value={formState.name || ''}  onChange={changeHandler} required></input>
+          <input type="email" name= "email" placeholder='Email id'  value={formState.email || ''}  onChange={changeHandler} required></input>
+          <input type="text" name= "phone" placeholder='Phone no.' value={formState.phone || ''}  onChange={changeHandler} required></input>
+          <textarea name = "message" rows="4" placeholder='How can I help you?' value={formState.message || ''}  onChange={changeHandler}></textarea>
           <button type='submit'>Send</button>
           </Form>
         </div>
